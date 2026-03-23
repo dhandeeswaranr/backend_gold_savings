e0e9043d7fef07a396526395f266a855ea483ccc
Kaviya-S-11 <kaviyasakthivel115@gmail.com>
2026-03-23 12:53:13 +0530

    localhostID

--- src/db.ts
+++ src/db.ts
@@ -2,26 +2,28 @@ import { Pool } from 'pg';
 /**L
  * Local database connection
  */
-/*const pool = new Pool({
+
+const pool = new Pool({
   host: 'localhost',
   user: 'postgres',
   password: 'postgres',
-  database: 'school',
+  database: 'jewel',
   port: 5432,
-});*/
+});
 
 /**
  * Render.com database connection
  */
-const pool = new Pool({
-  host: 'dpg-d6p8emf5r7bs73fc4dag-a.oregon-postgres.render.com',
-  user: 'dhandy_0',
-  password: 'vViXdHyxQb3AZKeUIYqlPfdoRrbrKyIe',
-  database: 'school_6ybj',
-  port: 5432,
-   ssl: {
-    rejectUnauthorized: false
-  }
-});
+// const pool = new Pool({
+//   host: 'dpg-d6p8emf5r7bs73fc4dag-a.oregon-postgres.render.com',
+//   user: 'dhandy_0',
+//   password: 'vViXdHyxQb3AZKeUIYqlPfdoRrbrKyIe',
+//   database: 'school_6ybj',
+//   port: 5432,
+//    ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
 
 export default pool;
