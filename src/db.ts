aab0be1592a3f29a813a1d450cc87bd3ffa7367d
dhandy <dhandy.raju@swireshipping.com>
2026-03-23 19:42:10 +0530

    new db

--- src/db.ts
+++ src/db.ts
@@ -13,15 +13,26 @@ import { Pool } from 'pg';
 /**
  * Render.com database connection
  */
+// const pool = new Pool({
+//   host: 'dpg-d6p8emf5r7bs73fc4dag-a.oregon-postgres.render.com',
+//   user: 'dhandy_0',
+//   password: 'vViXdHyxQb3AZKeUIYqlPfdoRrbrKyIe',
+//   database: 'school_6ybj',
+//   port: 5432,
+//    ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+
 const pool = new Pool({
-  host: 'dpg-d6p8emf5r7bs73fc4dag-a.oregon-postgres.render.com',
-  user: 'dhandy_0',
-  password: 'vViXdHyxQb3AZKeUIYqlPfdoRrbrKyIe',
-  database: 'school_6ybj',
+  host: 'dpg-d70kjdhr0fns73ctnim0-a.oregon-postgres.render.com',
+  user: 'vj_test_user',
+  password: 'n7f9cihb4PfxRC5XjxOIubUXsU3OQf4k',
+  database: 'vj_test',
   port: 5432,
    ssl: {
     rejectUnauthorized: false
   }
 });
-
 export default pool;
